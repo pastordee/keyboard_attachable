d8f3c2223f013127c4c578b3c18c97dd45899c70
pastordee <davidfrimpong@me.com>
2024-04-20 16:28:01 +0100

    universal_platform
    added universal_platform because Platform.isAndroid is dose not work on the web

--- lib/src/animation/keyboard_animation_injector.dart
+++ lib/src/animation/keyboard_animation_injector.dart
@@ -4,14 +4,18 @@ import 'package:flutter/widgets.dart';
 import 'package:keyboard_attachable/src/animation/keyboard_animation_controller.dart';
 import 'package:keyboard_attachable/src/animation/platform_controllers/android_keyboard_animation_controller.dart';
 import 'package:keyboard_attachable/src/animation/platform_controllers/ios_keyboard_animation_controller.dart';
+import 'package:universal_platform/universal_platform.dart';
 
 class KeyboardAnimationInjector {
   const KeyboardAnimationInjector(this._tickerProvider);
 
   final TickerProvider _tickerProvider;
 
+  
+
   KeyboardAnimationController getPlatformController() {
-    if (Platform.isAndroid) {
+    // bool isIos = UniversalPlatform.isIOS;
+    if (UniversalPlatform.isAndroid) {
       return AndroidKeyboardAnimationController(vsync: _tickerProvider);
     } else {
       return IOSKeyboardAnimationController(vsync: _tickerProvider);
